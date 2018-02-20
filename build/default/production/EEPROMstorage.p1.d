8af6df959127ff8a593675715e675c5646870911
chrismazzuca <chrismazz-7@live.com>
2018-02-19 21:24:22 -0500

    Initial EEPROM

---
+++ build/default/production/EEPROMstorage.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/EEPROMstorage.d  \
+ build/default/production/EEPROMstorage.p1:  \
+ EEPROMstorage.c  \
+EEPROMstorage.h 
--- build/default/production/standbyMode.p1.d
+++ build/default/production/standbyMode.p1.d
@@ -1,5 +1,6 @@
  build/default/production/standbyMode.d  \
  build/default/production/standbyMode.p1:  \
  standbyMode.c  \
+EEPROMstorage.h  \
 standbyMode.h  \
 configBits.h 
